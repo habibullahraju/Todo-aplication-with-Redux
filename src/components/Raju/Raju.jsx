48d9cb0c240acd5eb340a277b76e69baed242b72
habibullahraju <raju.ra423@gmail.com>
2023-11-05 00:38:35 +0600

    i a file

--- src/components/Raju/Raju.jsx
+++ src/components/Raju/Raju.jsx
@@ -3,7 +3,8 @@ import React from 'react';
 const Raju = () => {
     return (
         <div>
-            <h2>Hello Raju</h2>
+            <h2>Hello world</h2>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nesciunt provident, aut dolore perspiciatis ducimus excepturi tempora magnam eveniet quisquam expedita impedit earum quam sit repudiandae inventore? Facilis minus magni earum omnis eligendi illo, obcaecati repellat. Accusantium facere repellat eius dicta rem quidem optio neque quasi eveniet, suscipit voluptatum perferendis magni.</p>
         </div>
     );
 };
